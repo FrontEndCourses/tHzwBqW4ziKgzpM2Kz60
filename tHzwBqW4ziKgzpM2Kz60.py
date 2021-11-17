91a9d6492ac93a57570226a43a55728a6f3d4a4a
ZHANG Ning <zhn038@gmail.com>
2021-11-17 10:31:44 +0800

    Create tHzwBqW4ziKgzpM2Kz60.py

---
+++ tHzwBqW4ziKgzpM2Kz60.py
@@ -0,0 +1,13 @@
+import urllib.request
+get_this_url = "https://jt.ximalaya.com//CKwRIaIE8v7hAEwJ0ADZZ5Un.m4a?channel=rss&album_id=40907109&track_id=445136660&uid=250932269&jt=http://audio.xmcdn.com/storages/2e49-audiofreehighqps/8C/43/CKwRIaIE8v7hAEwJ0ADZZ5Un.m4a"
+urllib.request.urlretrieve(get_this_url, "1")
+get_this_url = "https://jt.ximalaya.com//CKwRIJIE98coAF7mBwDayw4Z.m4a?channel=rss&album_id=40907109&track_id=446305795&uid=250932269&jt=http://audio.xmcdn.com/storages/0678-audiofreehighqps/8F/F4/CKwRIJIE98coAF7mBwDayw4Z.m4a"
+urllib.request.urlretrieve(get_this_url, "1")
+get_this_url = "https://jt.ximalaya.com//CKwRIW4FARgZAPyeGQDdk3WF.m4a?channel=rss&album_id=40907109&track_id=448581544&uid=250932269&jt=http://audio.xmcdn.com/storages/123b-audiofreehighqps/40/97/CKwRIW4FARgZAPyeGQDdk3WF.m4a"
+urllib.request.urlretrieve(get_this_url, "1")
+get_this_url = "https://jt.ximalaya.com//CKwRIRwEfCs7AAsuOACuCc7_.m4a?channel=rss&amp;album_id=49547055&amp;track_id=417132342&amp;uid=250932269&amp;jt=http://audio.xmcdn.com/storages/80cb-audiofreehighqps/D5/D5/CKwRIRwEfCs7AAsuOACuCc7_.m4a"
+urllib.request.urlretrieve(get_this_url, "1")
+get_this_url = "https://jt.ximalaya.com//CKwRIJEEjBKLABCbXQCz0Zaz.m4a?channel=rss&amp;album_id=49547055&amp;track_id=420806006&amp;uid=250932269&amp;jt=http://audio.xmcdn.com/storages/fb60-audiofreehighqps/A0/00/CKwRIJEEjBKLABCbXQCz0Zaz.m4a"
+urllib.request.urlretrieve(get_this_url, "1")
+get_this_url = "https://jt.ximalaya.com//CKwRINsEfXocAAgzSACuc5TX.m4a?channel=rss&amp;album_id=49547055&amp;track_id=417416930&amp;uid=250932269&amp;jt=http://audio.xmcdn.com/storages/1244-audiofreehighqps/B7/90/CKwRINsEfXocAAgzSACuc5TX.m4a"
+urllib.request.urlretrieve(get_this_url, "1")
